46e3aff8a27e5978497fbaeea63ee070702ae60f
Andrew Fairweather <adacooly@gmail.com>
2017-09-25 16:20:36 +1300

    Change to test view when easy or hard button press

--- src/main/java/tatai/Application.java
+++ src/main/java/tatai/Application.java
@@ -46,11 +46,45 @@ public class Application extends javafx.application.Application {
 
     // Called when the easy button has been pressed
     private void easyTest() {
-        throw new NotImplementedException();
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/tatai/Test.fxml"));
+        LevelController controller = LevelControllerFactory.getLevelController("Easy");
+        loader.setController(controller);
+        
+        Stage stage = (Stage) easyBtn.getScene().getWindow();
+       
+        Parent root;
+        try {
+        	root = loader.load();
+        } catch(IOException e) {
+        	throw new RuntimeException("Unable to load tatai.Test.fxml");
+        }
+        
+        _scene = new Scene(root);
+        stage.setTitle(APP_NAME);
+        stage.setScene(_scene);
+        
+        stage.show();
     }
     // Called when the hard button has been pressed
     private void hardTest() {
-        throw new NotImplementedException();
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/tatai/Test.fxml"));
+        LevelController controller = LevelControllerFactory.getLevelController("Hard");
+        loader.setController(controller);
+        
+        Stage stage = (Stage) easyBtn.getScene().getWindow();
+       
+        Parent root;
+        try {
+        	root = loader.load();
+        } catch(IOException e) {
+        	throw new RuntimeException("Unable to load tatai.Test.fxml");
+        }
+        
+        _scene = new Scene(root);
+        stage.setTitle(APP_NAME);
+        stage.setScene(_scene);
+        
+        stage.show();
     }
     // Called when the info button has been pressed
     private void info() {
