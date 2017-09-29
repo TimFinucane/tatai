c70cd97341e6f28b2ef6422a86f87173786682cd
Andrew Fairweather <adacooly@gmail.com>
2017-09-29 21:41:25 +1300

    Create model class for easy level

--- src/main/java/tatai/EasyTestModel.java
+++ src/main/java/tatai/EasyTestModel.java
@@ -3,20 +3,18 @@ package tatai;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Controller class for the easy level.
- * @author andy
- *
+ * Model class for the easy level.
  */
 
-public class EasyLevelController extends LevelController {
+public class EasyTestModel extends TestModel {
 
 	private static final int MINTESTVALUE = 1;
 	private static final int MAXTESTVALUE = 9;
 	
 	// Returns a random number between the minumum test value and the maximum test value.
-	@Override
 	public int getRandom() {
 		return ThreadLocalRandom.current().nextInt(MINTESTVALUE, MAXTESTVALUE + 1);
 	}
+	
 	
 }
