6aeae5eecf9423fe39417ceb3d77c14787144d03
Andrew Fairweather <adacooly@gmail.com>
2017-09-29 21:42:31 +1300

    Create model class for Hard level

--- src/main/java/tatai/HardTestModel.java
+++ src/main/java/tatai/HardTestModel.java
@@ -3,16 +3,15 @@ package tatai;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Controller class for the hard level.
+ * Model class for the hard level.
  */
 
-public class HardLevelController extends LevelController{
+public class HardTestModel extends TestModel{
 	
 	private static final int MINTESTVALUE = 1;
 	private static final int MAXTESTVALUE = 99;
 	
 	// Returns a random number between the minimum test value and the maximum test value.
-	@Override
 	public int getRandom() {
 		return ThreadLocalRandom.current().nextInt(MINTESTVALUE, MAXTESTVALUE + 1);
 	}
