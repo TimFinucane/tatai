07fe45cc0fef695ec324f3262ff32d1b86d5d0a9
Andrew Fairweather <adacooly@gmail.com>
2017-09-29 21:40:46 +1300

    Create base TestModel class

---
+++ src/main/java/tatai/TestModel.java
@@ -0,0 +1,17 @@
+package tatai;
+
+/**
+ * Base model class for testing.
+ */
+
+public abstract class TestModel{
+
+	private int _score;
+		
+	public abstract int getRandom();
+	
+	
+	//TODO: Add score counter.
+	//TODO: Add logic for when answer is right and wrong.
+
+}
