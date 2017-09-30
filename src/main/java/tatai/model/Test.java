71aaa756edaac902ce44de2cbe8d3394ad63ddd9
Timothy Finucane <timfinucane@outlook.com>
2017-09-30 14:51:31 +1300

    Add todo comments on Test

--- src/main/java/tatai/model/Test.java
+++ src/main/java/tatai/model/Test.java
@@ -11,7 +11,8 @@ public abstract class Test {
 	public abstract int getRandom();
 	
 	
-	//TODO: Add score counter.
+	//TODO: Add score counter - potentially store score elsewhere for later access, but if necessary i can do this (Tim)
 	//TODO: Add logic for when answer is right and wrong.
-
+	//TODO: generate() the number, be able to return a (maori) string of that number, and verify(Recording) for -
+	//TODO:  whether or not it is valid (return boolean).
 }
