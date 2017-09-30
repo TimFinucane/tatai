b9e480f37d9553f86f4ad24734a0c17f91e5bc7e
Andrew Fairweather <adacooly@gmail.com>
2017-09-30 22:18:33 +1300

    Check if user has answered correctly or not

--- src/main/java/tatai/model/Test.java
+++ src/main/java/tatai/model/Test.java
@@ -6,13 +6,44 @@ package tatai.model;
 
 public abstract class Test {
 
-	private int _score;
-		
+	private int _score = 0;
+	private int _testValue;
+	private boolean _hasMultipleTries = true;	
+	
 	public abstract int getRandom();
 	
+	/**
+	 * Generates a random number for the question.
+	 * @return maori translation of that number.
+	 */
+	public String generate() {
+		_testValue = getRandom();
+		return Translator.convert(_testValue);
+	}
 	
-	//TODO: Add score counter - potentially store score elsewhere for later access, but if necessary i can do this (Tim)
-	//TODO: Add logic for when answer is right and wrong.
-	//TODO: generate() the number, be able to return a (maori) string of that number, and verify(Recording) for -
-	//TODO:  whether or not it is valid (return boolean).
+	/**
+	 * Method which compares the users answer to the expected result.
+	 * @param answer the users response 
+	 * @return true if the answer is equal to the expected result.
+	 */
+	public boolean verify(String answer) {
+		if(answer.equalsIgnoreCase((Translator.convert(_testValue))) {
+			_score++;
+			_hasMultipleTries = true;
+			return true;
+		}
+		else {
+			_hasMultipleTries = false;
+			return false;
+		}
+	}
+	
+	/**
+	 * Method which returns whether or not the user is on their first or second try.
+	 * @return true if they are on their first true or false if they are on their second try.
+	 */
+	public boolean hasMultpleTries() {
+		return _hasMultipleTries;
+	}
+	
 }
