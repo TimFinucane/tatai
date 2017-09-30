b4df8cf9a4f78792cd57c6a32efda15d074185e2
Timothy Finucane <timfinucane@outlook.com>
2017-09-30 18:07:24 +1300

    Replace text in PlaybackControl with graphics

--- src/main/java/tatai/PlaybackControl.java
+++ src/main/java/tatai/PlaybackControl.java
@@ -1,6 +1,8 @@
 package tatai;
 
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -10,18 +12,12 @@ import javafx.scene.media.MediaPlayer;
  */
 public class PlaybackControl extends Region {
     public PlaybackControl() {
-        _playbackBtn = new Button("Play");
+        _playbackBtn = new Button( "", _imageView );
         _playbackBtn.setDisable(true);
 
         getChildren().add(_playbackBtn);
 
-        _playbackBtn.setOnAction((ignored) -> {
-            if(!_playing) {
-                play();
-            } else {
-                stop();
-            }
-        });
+        _imageView.setPreserveRatio(true);
     }
 
     /**
@@ -41,20 +37,32 @@ public class PlaybackControl extends Region {
         _playbackBtn.setDisable(true);
     }
 
+    /**
+     * A special resize function which only takes one parameter that determines both the width and height
+     *  of the playback control.
+     */
+    public void     resize(double size) {
+        _imageView.setFitHeight(size);
+    }
+
     // Called when the play button is pressed
     private void play() {
         _player.play();
-        _playbackBtn.setText("Stop");
+        _imageView.setImage(_stopImage);
         _playing = true;
     }
     // Called when the 'stop' button is pressed
     private void stop() {
         _player.stop();
-        _playbackBtn.setText("Play");
+        _imageView.setImage(_playImage);
         _playing = false;
     }
 
     private boolean     _playing = false;
     private MediaPlayer _player;
     private Button      _playbackBtn;
+
+    private Image       _playImage = new Image(getClass().getResourceAsStream("/tatai/playback/play.png"));
+    private Image       _stopImage = new Image(getClass().getResourceAsStream("/tatai/playback/stop.png"));
+    private ImageView   _imageView = new ImageView(_playImage);
 }
