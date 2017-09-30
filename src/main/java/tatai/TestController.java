f3718f2815b79ce7c32d25e90de01542e1a2c8fa
Timothy Finucane <timfinucane@outlook.com>
2017-09-30 14:50:34 +1300

    Add automatic resizing to TestController

--- src/main/java/tatai/TestController.java
+++ src/main/java/tatai/TestController.java
@@ -12,6 +12,10 @@ import java.io.IOException;
  * A test window, to which you can pass specifications for the type of test
  */
 public class TestController extends VBox {
+    private static double NUMBER_HEIGHT_DIV = 5;
+    private static double NUMBER_WIDTH_DIV = 3;
+    private static double CONTROL_HEIGHT_DIV = 15;
+    private static double CONTROL_WIDTH_DIV = 12;
 
 	public TestController(Test model) {
 	    _model = model;
@@ -42,6 +46,20 @@ public class TestController extends VBox {
      *  was correct.
      */
     private void    endRound() {
+    }
+
+    @Override
+    public void    resize(double width, double height) {
+        super.resize(width, height);
+
+        // Adjust children to be approx. the right size based on window size
+        // These numbers are pretty good as they are
+        double numberSize = Math.min(getHeight()/NUMBER_HEIGHT_DIV, getWidth()/NUMBER_WIDTH_DIV);
+        double controlSize = Math.min(getHeight()/CONTROL_HEIGHT_DIV, getWidth()/CONTROL_WIDTH_DIV);
+
+        numberLbl.setStyle("-fx-font-size: " + numberSize);
+        playbackCntrl.setStyle("-fx-font-size: " + controlSize);
+        recorderCntrl.setStyle("-fx-font-size: " + controlSize);
     }
 
 	private Test    _model;
