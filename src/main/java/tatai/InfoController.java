20bebc40e6eea018504ab32a91ebf8cc219c4817
Timothy Finucane <timfinucane@outlook.com>
2017-10-02 15:09:19 +1300

    Add resizing to Info screen

--- src/main/java/tatai/InfoController.java
+++ src/main/java/tatai/InfoController.java
@@ -15,6 +15,11 @@ import java.util.List;
  * Shows statistics about the session score
  */
 public class InfoController extends VBox {
+    private static double TITLE_HEIGHT_DIV = 15;
+    private static double TITLE_WIDTH_DIV = 9;
+    private static double SMALL_HEIGHT_DIV = 24;
+    private static double SMALL_WIDTH_DIV = 15;
+
     /**
      * Creates an Info screen. Once the user is done, notifyFinished will be called
      */
@@ -41,6 +46,27 @@ public class InfoController extends VBox {
         mainMenuBtn.setOnAction((e) -> _notifyFinished.run());
 
         loadData();
+    }
+
+    /**
+     * Override normal resize to also scale elements
+     */
+    @Override
+    public void resize(double width, double height) {
+        super.resize(width, height);
+
+        double titleSize = Math.min(getHeight() / TITLE_HEIGHT_DIV, getWidth() / TITLE_WIDTH_DIV);
+        double smallSize = Math.min(getHeight() / SMALL_HEIGHT_DIV, getWidth() / SMALL_WIDTH_DIV);
+
+        titleLbl.setStyle("-fx-font-size: " + titleSize);
+
+        infoLbl.setStyle("-fx-font-size: " + smallSize);
+        highLbl.setStyle("-fx-font-size: " + smallSize);
+        avgLbl.setStyle("-fx-font-size: " + smallSize);
+        highEasyLbl.setStyle("-fx-font-size: " + smallSize);
+        highHardLbl.setStyle("-fx-font-size: " + smallSize);
+        avgEasyLbl.setStyle("-fx-font-size: " + smallSize);
+        avgHardLbl.setStyle("-fx-font-size: " + smallSize);
     }
 
     private void loadData() {
@@ -83,6 +109,15 @@ public class InfoController extends VBox {
     }
 
     private Runnable _notifyFinished;
+
+    @FXML
+    private Label   titleLbl;
+    @FXML
+    private Label   infoLbl;
+    @FXML
+    private Label   highLbl;
+    @FXML
+    private Label   avgLbl;
 
     @FXML
     private Label   highEasyLbl;
