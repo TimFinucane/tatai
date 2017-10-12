0041539f50c53c8f7fb230962a3a72bd1e626d6c
Andrew Fairweather <adacooly@gmail.com>
2017-10-12 23:38:05 +1300

    Change the icons for the record control

--- src/main/java/tatai/RecorderControl.java
+++ src/main/java/tatai/RecorderControl.java
@@ -1,10 +1,14 @@
 package tatai;
 
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.media.Media;
+import javafx.scene.paint.Paint;
 import tatai.model.Recognizer;
 import tatai.model.Recording;
 
@@ -16,8 +20,12 @@ import java.util.function.Consumer;
  * recorded item
  */
 public class RecorderControl extends Region {
+
     public RecorderControl() {
-        _button = new Button("", _imageView);
+
+        _button = new JFXButton("");
+        _button.setGraphic(setRecord());
+
         getChildren().add(_button);
 
         // Play when the button is pressed first, then Stop on the second time, repeat.
@@ -36,8 +44,6 @@ public class RecorderControl extends Region {
             }
         }));
 
-        _imageView.setPreserveRatio(true);
-        getStyleClass().add("recorder");
     }
 
     /**
@@ -63,16 +69,17 @@ public class RecorderControl extends Region {
      * A special resize function which only takes one parameter that determines both the width and height
      *  of the icons control.
      */
-    public void     resize(double size) {
-        _imageView.setFitHeight(size);
-    }
+  //  public void     resize(double size) {
+   //     _imageView.setFitHeight(size);
+  // }
 
     // Starts a recording. Is called when button is pressed
     private void    start() {
         if( _recording != null && !_recording.stopped()) {
             _recording.stop();
         }
-        _imageView.setImage(_stopImage);
+        //_imageView.setImage(_stopImage);
+        _button.setGraphic(setStop());
         _recording = Recording.start();
 
         if(_recordingStarted != null) {
@@ -81,7 +88,8 @@ public class RecorderControl extends Region {
     }
     // Stops that recording. Called when button pressed again
     private void    stop() {
-        _imageView.setImage(_recordImage);
+        //_imageView.setImage(_recordImage);
+        _button.setGraphic(setRecord());
         _recording.stop();
 
         if(_mediaAvailable != null) {
@@ -92,6 +100,21 @@ public class RecorderControl extends Region {
         Recognizer.recognize(_recording.fileName(), _recognizerCompleted);
     }
 
+//    Just some helper methods to create the icon to be displayed.
+    private static FontAwesomeIconView setRecord() {
+        FontAwesomeIconView icon = new FontAwesomeIconView(FontAwesomeIcon.CIRCLE);
+        icon.setFill(Paint.valueOf("#e74c3c"));
+        icon.setGlyphSize(150);
+        return icon;
+    }
+
+    private static FontAwesomeIconView setStop() {
+        FontAwesomeIconView icon = new FontAwesomeIconView(FontAwesomeIcon.STOP);
+        icon.setFill(Paint.valueOf("#e74c3c"));
+        icon.setGlyphSize(150);
+        return icon;
+    }
+
     private Button      _button;
     private Recording   _recording = null;
 
@@ -99,7 +122,7 @@ public class RecorderControl extends Region {
     private Runnable            _recordingStarted = null;
     private Consumer<String>    _recognizerCompleted = null;
 
-    private Image       _recordImage = new Image(getClass().getResourceAsStream("/tatai/icons/record.png"));
-    private Image       _stopImage = new Image(getClass().getResourceAsStream("/tatai/icons/stop.png"));
-    private ImageView   _imageView = new ImageView(_recordImage);
+    //private Image       _recordImage = new Image(getClass().getResourceAsStream("/tatai/icons/record.png"));
+    //private Image       _stopImage = new Image(getClass().getResourceAsStream("/tatai/icons/stop.png"));
+    //private ImageView   _imageView = new ImageView(_recordImage);
 }
