b4db42aabd6f9a53c2fbe07c9bfd19b865a5fbd2
Andrew Fairweather <adacooly@gmail.com>
2017-10-12 23:38:31 +1300

    Change the icons for the playback control

--- src/main/java/tatai/PlaybackControl.java
+++ src/main/java/tatai/PlaybackControl.java
@@ -1,24 +1,29 @@
 package tatai;
 
+import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Paint;
 
 /**
  * A control that plays sound media (no visuals)
  */
 public class PlaybackControl extends Region {
     public PlaybackControl() {
-        _playbackBtn = new Button( "", _imageView );
+        _playbackBtn = new JFXButton( "");
+
+        _playbackBtn.setGraphic(setPlay());
         _playbackBtn.setDisable(true);
 
         getChildren().add(_playbackBtn);
 
-        _imageView.setPreserveRatio(true);
-        getStyleClass().add("playback");
+       // _imageView.setPreserveRatio(true);
 
         _playbackBtn.setOnAction((ignored) -> {
             if(!_playing) {
@@ -61,28 +66,45 @@ public class PlaybackControl extends Region {
      * A special resize function which only takes one parameter that determines both the width and height
      *  of the icons control.
      */
-    public void     resize(double size) {
-        _imageView.setFitHeight(size);
-    }
+    //public void     resize(double size) {
+  //      _imageView.setFitHeight(size);
+  //  }
 
     // Called when the play button is pressed
     private void play() {
         _player.play();
-        _imageView.setImage(_stopImage);
+        //_imageView.setImage(_stopImage);
+        _playbackBtn.setGraphic(setStop());
         _playing = true;
     }
     // Called when the 'stop' button is pressed
     private void stop() {
         _player.stop();
-        _imageView.setImage(_playImage);
+        _playbackBtn.setGraphic(setPlay());
+        //_imageView.setImage(_playImage);
         _playing = false;
+    }
+
+//    Helper methods to display icons.
+    private static FontAwesomeIconView setPlay() {
+        FontAwesomeIconView icon = new FontAwesomeIconView(FontAwesomeIcon.PLAY);
+        icon.setFill(Paint.valueOf("#2ecc71"));
+        icon.setGlyphSize(150);
+        return icon;
+    }
+
+    private static FontAwesomeIconView setStop() {
+        FontAwesomeIconView icon = new FontAwesomeIconView(FontAwesomeIcon.STOP);
+        icon.setFill(Paint.valueOf("#e74c3c"));
+        icon.setGlyphSize(150);
+        return icon;
     }
 
     private boolean     _playing = false;
     private MediaPlayer _player = null;
     private Button      _playbackBtn;
 
-    private Image       _playImage = new Image(getClass().getResourceAsStream("/tatai/icons/play.png"));
-    private Image       _stopImage = new Image(getClass().getResourceAsStream("/tatai/icons/stop.png"));
-    private ImageView   _imageView = new ImageView(_playImage);
+    //private Image       _playImage = new Image(getClass().getResourceAsStream("/tatai/icons/play.png"));
+  //  private Image       _stopImage = new Image(getClass().getResourceAsStream("/tatai/icons/stop.png"));
+   // private ImageView   _imageView = new ImageView(_playImage);
 }
