bc9b68d5b30676b2a0c7dd3f52efad6fee907482
Timothy Finucane <timfinucane@outlook.com>
2017-10-20 16:37:12 +1300

    Improve Application logic

--- src/main/java/tatai/Application.java
+++ src/main/java/tatai/Application.java
@@ -22,30 +22,27 @@ public class Application extends javafx.application.Application implements Sideb
         setUserAgentStylesheet("/tatai/stylesheets/DarkMode.css");
 
         // Have a sidebar on the left, and a pane in the centre to add our screens
-        sidebar = new Sidebar(this);
-        centre = new HBox();
-        centre.setPrefWidth(500.0);
-        centre.setAlignment(Pos.CENTER);
+        _sidebar = new Sidebar(this);
+        _centre = new HBox();
+        _centre.setPrefWidth(500.0);
+        _centre.setAlignment(Pos.CENTER);
 
         BorderPane pane = new BorderPane();
-        pane.setLeft(sidebar);
-        pane.setCenter(centre);
+        pane.setLeft(_sidebar);
+        pane.setCenter(_centre);
 
         stage.setScene(new Scene(pane));
 
         stage.setTitle(APP_NAME);
         stage.show();
 
-        centre.requestFocus(); // And return focus to us
+        _centre.requestFocus(); // And return focus to us
     }
 
     // TODO: Either lock the sidepane buttons or hide them when in test mode.
-    // TODO: Fix overlap if test or practice button pressed multiple times.
     // Called when home button pressed. Sends user to login screen
     @Override
     public void home(){
-        clearCentre();
-
         LoginController login;
         if(_user == null)
             login = new LoginController();
@@ -53,41 +50,33 @@ public class Application extends javafx.application.Application implements Sideb
             login = new LoginController(_user);
 
         // TODO: Account for user exiting without wanting to log in
-        login.display(centre, () -> {
+        setScreen(login, () -> {
             _user = login.name();
-            sidebar.unlockButtons();
+            _sidebar.unlockButtons();
         });
     }
 
     // Called when practice button pressed
     @Override
     public void practice() {
-        clearCentre();
-
-        new SelectTestController(true, false).display(centre, this::home);
+        setScreen(new SelectTestController(true, false));
     }
 
     // Called when test button pressed
     @Override
     public void test(){
-        clearCentre();
-
-        new SelectTestController().display(centre, this::home);
+        setScreen(new SelectTestController());
     }
 
     // Called when stats button pressed
     @Override
     public void stats(){
-        clearCentre();
-
-        new SelectTestController(false, true).display(centre, this::home);
+        setScreen(new SelectTestController(false, true));
     }
 
     // Called when the info button has been pressed
     @Override
     public void info() {
-        clearCentre();
-
         ButtonType light = new ButtonType("Light");
         ButtonType dark = new ButtonType("Dark");
 
@@ -101,13 +90,17 @@ public class Application extends javafx.application.Application implements Sideb
         });
     }
 
-    private void clearCentre() {
-        centre.getChildren().clear();
+    private void setScreen(Controller controller) {
+        setScreen(controller, () -> {});
+    }
+    private void setScreen(Controller controller, Runnable onExit) {
+        _centre.getChildren().clear();
+        controller.display(_centre, onExit);
     }
 
     private static final String APP_NAME = "T\u0101tai";
 
-    private String    _user = null;
-    private HBox      centre;
-    private Sidebar   sidebar;
+    private String  _user = null;
+    private HBox    _centre;
+    private Sidebar _sidebar;
 }
