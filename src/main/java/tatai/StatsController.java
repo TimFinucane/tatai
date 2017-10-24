e381ded2f4d0d6a38d2e598c355147d9f1bbfdf2
Andrew Fairweather <adacooly@gmail.com>
2017-10-25 12:16:25 +1300

    Fix stats window sizing

--- src/main/java/tatai/StatsController.java
+++ src/main/java/tatai/StatsController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import tatai.model.ScoreKeeper;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Class which displays the scores for the current test selected
  */
@@ -12,26 +15,38 @@ import tatai.model.ScoreKeeper;
 // TODO: Add other stats such as if the user is better at multiplication than addition etc.
 public class StatsController extends Controller {
     public StatsController(ScoreKeeper scoreKeeper, String testName){
-        dateBox.getStylesheets().add("/tatai/stylesheets/DarkMode.css");
-        scoreBox.getStylesheets().add("/tatai/stylesheets/DarkMode.css");
-
-        _scoreKeeper = scoreKeeper;
-        _testName = testName;
 
         loadFxml("Stats");
-
+        s = testScores();
         displayScores();
     }
 
-    private void displayScores() {
-        ScoreKeeper.Score[] scores = _scoreKeeper.getScores(_testName);
-        for(ScoreKeeper.Score curScore : scores) {
-            Label date = new Label(curScore.date.toString());
-            Label score = new Label(Integer.toString(curScore.score));
-            dateBox.getChildren().add(date);
-            scoreBox.getChildren().add(score);
-        }
+    private ScoreKeeper.Score[] testScores() {
+        ScoreKeeper scores = new ScoreKeeper("user1");
+
+        scores.addScore("test", 10);
+        scores.addScore("test", 12);
+
+        // Now reset and reload the score keeper
+
+        scores = new ScoreKeeper("user1");
+
+        ScoreKeeper.Score[] out = scores.getScores("test");
+        return out;
+
     }
+
+    private void displayScores() {
+        Label date = new Label(s[0].date.toString());
+        date.getStyleClass().add("-fx-background-color: ff0000");
+        date.getStyleClass().add("-fx-font-size: 18");
+        Label score = new Label(Integer.toString(s[0].score));
+        score.getStyleClass().add("-fx-background-color: ff0000");
+        dateBox.getChildren().add(date);
+        scoreBox.getChildren().add(score);
+    }
+
+    private static ScoreKeeper.Score[] s;
 
     private ScoreKeeper _scoreKeeper;
     private String      _testName;
