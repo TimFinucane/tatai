0c4c6f9c00358520cf18b354828a869c172562f7
Andrew Fairweather <adacooly@gmail.com>
2017-10-26 15:52:28 +1300

    Add progress bar to stats

--- src/main/java/tatai/StatsController.java
+++ src/main/java/tatai/StatsController.java
@@ -1,10 +1,12 @@
 package tatai;
 
+import com.jfoenix.controls.JFXProgressBar;
 import eu.hansolo.medusa.Gauge;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import tatai.model.test.TestJson;
 import tatai.model.user.ScoreKeeper;
 import tatai.model.user.User;
 
@@ -13,17 +15,22 @@ import tatai.model.user.User;
  */
 public class StatsController extends Controller {
 
-    public StatsController(ScoreKeeper scoreKeeper){
+    public StatsController(ScoreKeeper scoreKeeper, TestJson test){
         loadFxml("Stats");
+        _test = test;
         _scores = scoreKeeper.getScores();
 
         dateBox.getChildren().add(new Label("Date"));
         scoreBox.getChildren().add(new Label("Score"));
+        progressBox.getChildren().add(new Label("Result"));
         displayScores();
 
     }
 
     private void displayScores() {
+        int maxQuestions = 0;
+        for(TestJson.Question q : _test.questions)
+            maxQuestions += q.rounds;
 
         double average = 0.0;
         int i;
@@ -33,6 +40,10 @@ public class StatsController extends Controller {
             Label score = new Label(Integer.toString(_scores[i].score) );
             Label date = new Label(_scores[i].date.toString());
 
+            JFXProgressBar bar = new JFXProgressBar();
+            bar.setProgress((double)_scores[i].score/maxQuestions);
+
+            progressBox.getChildren().add(bar);
             dateBox.getChildren().add(date);
             scoreBox.getChildren().add(score);
 
@@ -53,6 +64,7 @@ public class StatsController extends Controller {
 
     private static final String COLOUR_VALUE =  "#22A7F0";
     private static User.Score[]  _scores;
+    private TestJson _test;
 
     // JavaFx controls
     @FXML private Gauge     gaugeValue;
