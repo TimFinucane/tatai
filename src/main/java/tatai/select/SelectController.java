73c5c59a03c3d334fb812f7391a28ef8a8f5e773
Timothy Finucane <timfinucane@outlook.com>
2017-10-27 11:30:50 +1300

    Remove temporary test creation

--- src/main/java/tatai/select/SelectController.java
+++ src/main/java/tatai/select/SelectController.java
@@ -18,7 +18,6 @@ import java.io.FileNotFoundException;
 public abstract class SelectController extends Controller {
     SelectController(User user, String title) {
         loadFxml("select/Select");
-        createBasicTests();
 
         this.user = user;
         this.titleLbl.setText(title);
@@ -64,88 +63,6 @@ public abstract class SelectController extends Controller {
             else
                 flowPane.getChildren().add(button);
         }
-    }
-
-    /**
-     * Temporary method for creating basic tests if they are not already made
-     */
-    private static void createBasicTests() {
-        if(Files.listTests().contains("Easy"))
-            return;
-
-        TestJson basic = new TestJson();
-        basic.custom = false;
-        basic.author = "Tatai";
-        basic.practice = false;
-
-        basic.name = "Easy";
-        basic.order = 0;
-        basic.questions = new TestJson.Question[1];
-        basic.questions[0] = new TestJson.Question();
-        basic.questions[0].rounds = 10;
-        basic.questions[0].tries = 2;
-        basic.questions[0].question = "(1 to 9) [\uFF0B] (1 to 9)";
-        basic.questions[0].min = 1;
-        basic.questions[0].max = 9;
-
-        TestParser.save(basic);
-
-        basic.name = "Hard";
-        basic.order = 1;
-        basic.randomizeQuestions = true;
-        basic.questions = new TestJson.Question[3];
-        basic.questions[0] = new TestJson.Question();
-        basic.questions[0].rounds = 4;
-        basic.questions[0].tries = 2;
-        basic.questions[0].question = "(1 to 99) [\uFF0B] (1 to 99)";
-        basic.questions[0].min = 1;
-        basic.questions[0].max = 99;
-
-        basic.questions[1] = new TestJson.Question();
-        basic.questions[1].rounds = 3;
-        basic.questions[1].tries = 2;
-        basic.questions[1].question = "(1 to 9) [\u00D7, \uFF0B] (1 to 9)";
-        basic.questions[1].min = 1;
-        basic.questions[1].max = 99;
-
-        basic.questions[2] = new TestJson.Question();
-        basic.questions[2].rounds = 3;
-        basic.questions[2].tries = 2;
-        basic.questions[2].question = "(10 to 99) [\u00F7] (1 to 9)";
-        basic.questions[2].min = 1;
-        basic.questions[2].max = 99;
-
-        basic.prerequisites = new TestJson.Prerequisite[] { new TestJson.Prerequisite("Easy", 8, 1) };
-
-        TestParser.save(basic);
-
-        basic.randomizeQuestions = false;
-        basic.practice = true;
-        basic.prerequisites = null;
-
-        basic.name = "Simple";
-        basic.order = 0;
-        basic.questions = new TestJson.Question[1];
-        basic.questions[0] = new TestJson.Question();
-        basic.questions[0].rounds = 10;
-        basic.questions[0].tries = 2;
-        basic.questions[0].question = "(1 to 9)";
-        basic.questions[0].min = 1;
-        basic.questions[0].max = 9;
-
-        TestParser.save(basic);
-
-        basic.name = "Advanced";
-        basic.order = 1;
-        basic.questions = new TestJson.Question[1];
-        basic.questions[0] = new TestJson.Question();
-        basic.questions[0].rounds = 10;
-        basic.questions[0].tries = 2;
-        basic.questions[0].question = "(1 to 99)";
-        basic.questions[0].min = 1;
-        basic.questions[0].max = 99;
-
-        TestParser.save(basic);
     }
 
     protected final User    user;
